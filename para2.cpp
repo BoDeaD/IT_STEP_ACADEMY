fcc5056cbdf36046c15f73de7c4526c1b402cfc6
BoDeaD <lageroped7@gmail.com>
2023-04-12 19:46:42 +0300

    para2

---
+++ para2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+/*
+практика1: вывести на экране консоли надпись как на картинке выше
+*/
+int main() {
+    std::cout << "\n\t\t\"To be\n\t\t\tor not\n\t\t\t\tto be...\"\n\t\t\t\t\t\\ Shackespeare \\\n";
+}
